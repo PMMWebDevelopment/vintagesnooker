50265bce9df9366f2dc974e6ed151b2a74efb5fa
P M Meddings <pmmeddings@gmail.com>
2018-06-13 11:01:06 +0100

    Delete connection.php

--- connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $link = mysqli_connect("localhost", "pmmweb5_vsadmin", "GaryOwen1963", "pmmweb5_vintagesnooker");
-    if(mysqli_connect_error()){
-        die ('ERROR: unable to connect to database: ' . mysqli_connect_error());
-    }
-?>
